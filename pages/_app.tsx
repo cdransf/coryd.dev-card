b514d4ad48197595e6ba0d20900707049273a5bb
Cory Dransfeldt <hi@coryd.dev>
2022-07-11 04:50:41 -0700

    wire up analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
 import '../styles/all.scss'
+import PlausibleScript from '@/components/analytics/Plausible'
 
 export default function MyCard({ Component, pageProps }) {
     return (
         <>
+            <PlausibleScript />
             <Component {...pageProps} />
         </>
     )
